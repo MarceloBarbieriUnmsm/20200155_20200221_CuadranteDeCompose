d15c25d92080d5b567305c5d4282f210c004c8bc
MarceloBarbieri <marcelo.barbieri@unmsm.edu.pe>
2023-08-30 00:00:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/a20200155_20200221_cuadrantedecompose/MainActivity.kt
@@ -0,0 +1,117 @@
+package com.example.a20200155_20200221_cuadrantedecompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.a20200155_20200221_cuadrantedecompose.ui.theme.CuadranteDeComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CuadranteDeComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CuadranteDeComposeApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CuadranteDeComposeApp() {
+
+    Column(Modifier.fillMaxWidth()){
+
+        Row(Modifier.weight(1f)){
+            ComposableInfoCard(
+                title = stringResource(R.string.primer_titulo),
+                description = stringResource(R.string.primer_cuerpo),
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.segundo_titulo),
+                description = stringResource(R.string.segundo_cuerpo),
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+        Row(Modifier.weight(1f)){
+            ComposableInfoCard(
+                title = stringResource(R.string.tercer_titulo),
+                description = stringResource(R.string.tercer_cuerpo),
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.cuarto_titulo),
+                description = stringResource(R.string.cuarto_cuerpo),
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+    }
+}
+
+@Composable
+private fun ComposableInfoCard(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CuadranteDeComposePreview() {
+    CuadranteDeComposeTheme {
+        CuadranteDeComposeApp()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "20200155_20200221_CuadranteDeCompose"
+include(":app")
+ 
